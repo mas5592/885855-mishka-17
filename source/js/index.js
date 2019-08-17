464060968662757727a5f14417beeec70097d1c9
mas5592 <mas5592@yandex.ru>
2019-08-17 23:51:21 +0300

    Внесение изменений в разметку

--- source/js/index.js
+++ source/js/index.js
@@ -1,19 +1,14 @@
 "use strict";
-
 var BREAKPOINTS = {
   mobile: "768px",
   tablet: "768px",
   desktop: "1150px"
 };
-
 var toggleButton = document.querySelector(".header-block__burger--js"),
   mainNav = document.querySelector(".header-nav__bottom");
-
-
 function loadShow() {
   toggleButton.classList.remove("visually-hidden");
 }
-
 function hideOnMedia(e) {
   if (e.matches) {
     mainNav.classList.remove("header-nav__bottom-js");
@@ -21,8 +16,7 @@ function hideOnMedia(e) {
     mainNav.classList.add("header-nav__bottom-js");
   }
 }
-
-toggleButton.addEventListener("click", function() {
+toggleButton.addEventListener("click", function () {
   this.classList.toggle("header-block__burger--close");
   mainNav.classList.toggle("header-nav__bottom-js--active");
 });
